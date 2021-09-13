5752e89dc5e00487369507b6d394667255f0efd6
leisiji <xuelinye9@gmail.com>
2021-09-14 07:59:39 +0800

    initial work for indent

--- init.lua
+++ init.lua
@@ -5,33 +5,44 @@ local function get_indent_size()
   return vim.bo.tabstop
 end
 
-local indent_size = get_indent_size()
+local indent = { '|', 'IndentBlanklineChar' }
+local blank = { ' ', '' }
 
 local mark = vim.api.nvim_buf_set_extmark
-local ns = vim.api.nvim_create_namespace("indent_line")
-vim.cmd[[highlight indent_line guifg=none gui=nocombine]]
+local ns = vim.api.nvim_create_namespace("indent_guides")
+vim.cmd[[highlight indent_line guifg=none guibg=none]]
 
-local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-for i, v in pairs(lines) do
-  if #v ~= 0 then
-    local idt = vim.fn.cindent(i)
-    if idt > 0 then
-      local indent = { '|', 'IndentBlanklineChar' }
-      local blank = { ' ', '' }
-      local guides = {}
-      local n = math.floor(idt / indent_size) - 1
+local M = {}
 
-      while n > 0 do
+local function create_mark(idt_size, ln)
+  local guides = {}
+  local idt = vim.fn.indent(ln)
+  if idt > 0 then
+    local n = math.floor(idt / idt_size)
+    print(ln, n)
+    if n > 1 then
+      for _ = 1, n - 1, 1 do
         guides[#guides+1] = indent
-        local j = indent_size - 1
-        while j > 0 do
+        for _ = 1, idt_size - 1, 1 do
           guides[#guides+1] = blank
-          j = j - 1
         end
-        n = n - 1
       end
 
-      mark(0, ns, i, 0, { virt_text = guides, virt_text_pos = 'overlay' })
+      mark(0, ns, ln - 1, idt_size, { virt_text = guides, virt_text_pos = 'overlay' })
     end
   end
 end
+
+function M.enable_indent_guides()
+ local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+ local idt_size = get_indent_size()
+  for i, v in pairs(lines) do
+    if #v ~= 0 then
+      create_mark(idt_size, i)
+    end
+  end
+end
+
+M.enable_indent_guides()
+
+return M
