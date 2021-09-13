7a315ab32115773ae6f5e0286d4f963d9b667394
leisiji <xuelinye9@gmail.com>
2021-09-13 20:06:24 +0800

    initial work for indent

--- init.lua
+++ init.lua
@@ -12,26 +12,26 @@ local ns = vim.api.nvim_create_namespace("indent_line")
 vim.cmd[[highlight indent_line guifg=none gui=nocombine]]
 
 local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+for i, v in pairs(lines) do
+  if #v ~= 0 then
+    local idt = vim.fn.cindent(i)
+    if idt > 0 then
+      local indent = { '|', 'IndentBlanklineChar' }
+      local blank = { ' ', '' }
+      local guides = {}
+      local n = math.floor(idt / indent_size) - 1
 
-for i, v in ipairs(lines) do
-  if #v == 0 then
-    return
-  end
+      while n > 0 do
+        guides[#guides+1] = indent
+        local j = indent_size - 1
+        while j > 0 do
+          guides[#guides+1] = blank
+          j = j - 1
+        end
+        n = n - 1
+      end
 
-  local indent = { "|", "IndentBlanklineChar" }
-  local blank = { "|", "IndentBlanklineChar" }
-  local guides = {}
-  local n = math.floor(vim.fn.cindent(i)) - 1
-  local j = indent_size
-
-  while n > 0 do
-    guides[#guides+1] = indent
-    while j > 0 do
-      guides[#guides+1] = blank
-      j = j - 1
+      mark(0, ns, i, 0, { virt_text = guides, virt_text_pos = 'overlay' })
     end
-    n = n - 1
   end
-  mark(0, ns, i, 0, { virt_text = guides, virt_text_pos = 'overlay' })
-
 end
